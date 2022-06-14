6c80f0ae0991527ecd361f48d15841eac37f6718
Rodrigo Simon <rodrigo.simon85@gmail.com>
2022-06-14 16:00:00 -0300

    updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,7 +25,7 @@ const Projects = () => (
         <BlogCard key={id}>
           <Img src={image} />
           <TitleContent>
-            <HeaderThree title>{title}</HeaderThree>
+            <HeaderThree $title>{title ? title : undefined}</HeaderThree>
             <Hr />
           </TitleContent>
           <CardInfo>{description}</CardInfo>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Built using the MERN stack as a final project for IGTI, it's a platform to list no longer used baby items, such as carts or toys, that can be of better use to other people who can't afford them, also contributing for a collaborative economy. Ongoing project.",
     image: '/images/img1.png',
-    tags: ['Mongo', 'Express', 'React', 'Node'],
+    tags: ['MongoDB', 'Express', 'React', 'Node'],
     source: 'https://github.com/rodsimon1/PA---IGTI',
     visit: 'https://projeto-aplicado-igti.netlify.app/',
     id: 0,
@@ -13,11 +13,20 @@ export const projects = [
     title: 'Memory Game',
     description:
       'A memory card game built with React making good use of React Hooks. Matched cards stay flipped, unmatched cards unflip, a turn counter marks your progress and there are three different difficulty choices.',
-    image: '/images/img2.png',
+    image: '/images/img2-2.png',
     tags: ['React', 'JavaScript'],
     source: 'https://github.com/rodsimon1/memory-game',
     visit: 'https://toon-memory-game.netlify.app/',
     id: 1,
+  },
+  {
+    title: 'Geofusion Front End Challenge',
+    description: 'Exercise challenge to list stores using the Leaflet Map, pagination, search terms and filtering.',
+    image: '/images/img6.png',
+    tags: ['React', 'HTML', 'CSS'],
+    source: 'https://github.com/rodsimon1/geofusion-challenge',
+    visit: 'https://geofusion-teste-rod.netlify.app/',
+    id: 2,
   },
   {
     title: 'Restaurant Landing Page',
@@ -27,15 +36,26 @@ export const projects = [
     tags: ['React', 'HTML', 'CSS'],
     source: 'https://github.com/rodsimon1/restaurant-landing-model',
     visit: 'https://restaurant-landing-model.netlify.app/',
-    id: 2,
+    id: 3,
   },
   {
+    title: 'Wordle Clone',
+    description:
+      'Clone of the popular game Wordle, with color coded characters in the grid and keypad, with a random solution being fetched from an API.',
+    image: '/images/img5.png',
+    tags: ['React', 'CSS', 'API'],
+    source: 'https://github.com/rodsimon1/wordle-clone',
+    visit: 'https://wordle-clone-rod.netlify.app/',
+    id: 4,
+  },
+
+  {
     title: 'React Calculator',
-    description: 'Built as an exercice, a working calculator using React.',
+    description: 'Built as an exercice, a a fully functional calculator using React, using reducers and reusable components',
     image: '/images/img4.png',
-    tags: ['React', 'CSS'],
+    tags: ['React', 'JavaScript', 'CSS'],
     source: 'https://github.com/rodsimon1/calculator-react',
     visit: 'https://react-calculator-rod.netlify.app/',
-    id: 2,
+    id: 5,
   },
 ];
